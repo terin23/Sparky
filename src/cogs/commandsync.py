7e6e700ad716b48bab655650173f5a5356a9cd5f
Terin <mlipnic@gmail.com>
2024-10-20 12:06:48 -0400

    Tested and confirmed working sparky.

--- src/cogs/commandsync.py
+++ src/cogs/commandsync.py
@@ -1,6 +1,6 @@
 from discord.ext import commands
 
-from src.config import NSC_ROLES
+
 
 
 class CommandSync(commands.Cog):
@@ -8,7 +8,6 @@ class CommandSync(commands.Cog):
         self.bot = bot
 
     @commands.command(name="commandsync")
-    @commands.has_any_role(*NSC_ROLES)
     async def commandsync(self, ctx):
         """Syncs the application commands."""
         await self.bot.tree.sync()
